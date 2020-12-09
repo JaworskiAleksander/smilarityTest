834bd01d7bee7d2b110e3b028685e9760ab8eec0
Aleksander Jaworski <aleck156@gmail.com>
2020-12-09 11:16:13 +0100

    initialize app and api, connect with mongoDB in app.py

--- web/app.py
+++ web/app.py
@@ -2,3 +2,9 @@ from flask import Flask, request, jsonify
 from flask_restful import Api, Resource
 from pymongo import MongoClient
 import bcrypt
+
+# initialize api
+app = Flask(__name__)
+api = Api(app)
+
+# connect to mongoDB
